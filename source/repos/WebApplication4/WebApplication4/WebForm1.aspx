cfd3516489d281bffb273ce97ea16cc785a8581a
Veeramahesh40 <veeramaheshreddy40@gmail.com>
2025-01-29 13:30:10 +0530

    add all fies in git hub

---
+++ source/repos/WebApplication4/WebApplication4/WebForm1.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <body>
+  <form id="form1" runat="server">  
+        <div>  
+            
+           <asp:HyperLink ID="HyperLink" runat="server" Text="LinkedIN" Font-Size="Medium" NavigateUrl="https://www.linkedin.com/in/veeramahesh-reddy-a0b4b320a/"></asp:HyperLink>
+            <br />
+            <br />
+            
+            <h2>Select your favorite computer brands:</h2>
+            <p>
+                <asp:CheckBox ID="apple" runat="server" Text="Apple" />
+            </p>
+            <p>
+                <asp:CheckBox ID="dell" runat="server" Text="Dell" />
+            </p>
+            <p>
+                <asp:CheckBox ID="lenevo" runat="server" Text="Lenovo" />
+            </p>
+            <p>
+                <asp:CheckBox ID="acer" runat="server" Text="Acer" />
+            </p>
+            <p>
+                <asp:CheckBox ID="sony" runat="server" Text="Sony" />
+            </p>
+            <p>
+                <asp:CheckBox ID="wipro" runat="server" Text="Wipro" />
+            </p>
+            <p>
+                <asp:Button ID="Button5" runat="server" Text="Submit" OnClick="Cookie_Click" />
+            </p>
+            <p>
+                <asp:Label ID="Label2" runat="server" Text="Please select your choice." ForeColor="Red" />
+            </p>
+            
+            <asp:Label ID="labelId" runat="server">User Name</asp:Label>  
+<asp:TextBox ID="UserName" runat="server" ToolTip="Enter User Name"></asp:TextBox>  
+
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  
+
+            <p>
+                <asp:RadioButton ID="Button1" runat="server" Text="Male" GroupName="Gender" />
+                <asp:RadioButton ID="Button2" runat="server" Text="Female" GroupName="Gender" />
+                <asp:RadioButton ID="Button3" runat="server" Text="Trans" GroupName="Gender" />
+            &nbsp;&nbsp;&nbsp;  
+        <asp:Button ID="SubmitButton" runat="server" Text="Submit" OnClick="SubmitButton_Click" />  
+            </p>
+            <h2>Select Courses</h2>
+            <p>
+                   
+            <asp:CheckBox ID="CheckBox1" runat="server" Text="J2SE" />  
+            <asp:CheckBox ID="CheckBox2" runat="server" Text="J2EE" />  
+            <asp:CheckBox ID="CheckBox3" runat="server" Text="Spring" />  
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  
+            <asp:Button ID="Button4" runat="server" Text="Button" OnClick="CheckButton" />  
+            </p>
+            <p>
+                <asp:Calendar ID="CalenderID" runat="server" OnSelectionChanged="Calenderclick" DayStyle-BackColor="#3399ff" DayHeaderStyle-BackColor="WindowFrame" DayHeaderStyle-ForeColor="#99ff66" ></asp:Calendar>
+            &nbsp;&nbsp;&nbsp;
+            </p>
+        </div>  
+      <p>
+     <asp:Label ID="userInput" runat="server" Text=""></asp:Label>  
+     <asp:Label ID="Showdate" runat="server" Text=""></asp:Label>
+        <asp:Label ID="Check" runat="server" Text=""></asp:Label>
+      </p>
+        <p>
+            &nbsp;</p>
+      <h2>Browse to Upload File</h2>
+      <p>
+            <asp:FileUpload ID="FileUpload1" runat="server" AllowMultiple="true" /> 
+      &nbsp;&nbsp;  
+            <asp:Button ID="File" runat="server" Text="Upload File" OnClick="FileUpload_Click" />  
+        &nbsp;  
+           <asp:Label runat="server" ID="FileUploadStatus" Text="" ForeColor="Red"></asp:Label> 
+      </p>
+        <p>  
+            &nbsp;</p>       
+       <p>  
+     <asp:Button ID="Download" runat="server" Text="Downloadfile" OnClick="Download_File" />  
+ &nbsp;  
+     <asp:Label runat="server" ID="Label1" Text="" ForeColor="Red"></asp:Label> 
+ </p>
+    </form>  
+        <p>  
+            &nbsp;</p> 
+          <p>  
+              &nbsp;</p> 
+</body>
+</html>
---
+++ source/repos/WebApplication4/WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Check if the cookie "computer" exists and its expiration date
+            if (Request.Cookies["computer"] != null)
+            {
+                // If the cookie exists, retrieve and display its values
+                Label2.Text = "Your choices: ";
+
+                if (Request.Cookies["computer"]["apple"] != null)
+                    Label2.Text += Request.Cookies["computer"]["apple"] + " ";
+                if (Request.Cookies["computer"]["dell"] != null)
+                    Label2.Text += Request.Cookies["computer"]["dell"] + " ";
+                if (Request.Cookies["computer"]["lenevo"] != null)
+                    Label2.Text += Request.Cookies["computer"]["lenevo"] + " ";
+                if (Request.Cookies["computer"]["acer"] != null)
+                    Label2.Text += Request.Cookies["computer"]["acer"] + " ";
+                if (Request.Cookies["computer"]["sony"] != null)
+                    Label2.Text += Request.Cookies["computer"]["sony"] + " ";
+                if (Request.Cookies["computer"]["wipro"] != null)
+                    Label2.Text += Request.Cookies["computer"]["wipro"] + " ";
+            }
+            else
+            {
+                Label2.Text = "Please select your choice.";
+            }
+        }
+        protected void Cookie_Click(object sender, EventArgs e)
+        {
+            // Initialize the cookie for storing selected choices
+            HttpCookie cookie = new HttpCookie("computer");
+
+            // Check each checkbox and add the selected choices to the cookie
+            if (apple.Checked)
+                cookie["apple"] = "apple";
+            if (dell.Checked)
+                cookie["dell"] = "dell";
+            if (lenevo.Checked)
+                cookie["lenevo"] = "lenevo";
+            if (acer.Checked)
+                cookie["acer"] = "acer";
+            if (sony.Checked)
+                cookie["sony"] = "sony";
+            if (wipro.Checked)
+                cookie["wipro"] = "wipro";
+
+            // Set the cookie expiration to 7 days
+            cookie.Expires = DateTime.Now.AddDays(7);
+
+            // Add the cookie to the response
+            Response.Cookies.Add(cookie);
+
+            // Refresh the page to display the updated choices
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            userInput.Text = "";
+
+            if (Button1.Checked)
+            {
+                userInput.Text = "your gender is : " + Button1.Text;
+            }
+            else if (Button2.Checked)
+            {
+                userInput.Text = "your gender is : " + Button2.Text;
+            }
+            else
+            {
+                userInput.Text = "your gender is : " + Button3.Text;
+            }
+        }
+
+        protected void Calenderclick(object sender, EventArgs e)
+        {
+            Showdate.Text = "Your selected : " + CalenderID.SelectedDate.ToString("M");
+        }
+        protected void CheckButton(object sender, EventArgs e)
+        {
+            var message = "";
+            if (CheckBox1.Checked)
+            {
+                message = CheckBox1.Text + " ";
+            }
+            if (CheckBox2.Checked)
+            {
+                message += CheckBox2.Text + " ";
+            }
+            if (CheckBox3.Checked)
+            {
+                message += CheckBox3.Text;
+            }
+            Check.Text = message;
+
+        }
+        protected void FileUpload_Click(object sender, EventArgs e)
+        {
+            if ((FileUpload1.PostedFile != null) && (FileUpload1.PostedFile.ContentLength > 0))
+            {
+                var count = 0;
+                foreach (HttpPostedFile uploadedFile in FileUpload1.PostedFiles)
+                {
+                    string fn = System.IO.Path.GetFileName(uploadedFile.FileName);
+                    string SaveLocation = Server.MapPath("upload") + "\\" + fn;
+                    try
+                    {
+                        uploadedFile.SaveAs(SaveLocation);
+                        count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        FileUploadStatus.Text = "Error: " + ex.Message;
+                    }
+                }
+                if (count > 0)
+                {
+                    FileUploadStatus.Text = count + " files has been uploaded.";
+                }
+            }
+            else
+            {
+                FileUploadStatus.Text = "Please select a file to upload.";
+            }
+           
+
+        }
+        protected void Download_File(object sender, EventArgs e)
+        {
+            string filePath = "D:\\Mahesh\\abc.txt";
+            FileInfo file = new FileInfo(filePath);
+            if (file.Exists)
+            {
+                // Clear Rsponse reference  
+                Response.Clear();
+                // Add header by specifying file name  
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+                // Add header for content length  
+                Response.AddHeader("Content-Length", file.Length.ToString());
+                // Specify content type  
+                Response.ContentType = "text/plain";
+                // Clearing flush  
+                Response.Flush();
+                // Transimiting file  
+                Response.TransmitFile(file.FullName);
+                Response.End();
+            }
+            else Label1.Text = "Requested file is not available to download";
+        }
+
+
+    }
+}
